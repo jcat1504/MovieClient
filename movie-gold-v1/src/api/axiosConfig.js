061a29c4c5e41a89915981176989214234998259
Jess Y <jyin978@hotmail.com>
2023-08-02 16:31:36 -0400

    test

--- movie-gold-v1/src/api/axiosConfig.js
+++ movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080',
+    // baseURL:'http://localhost:8080',
+    baseURL: 'https://movie-gold-v2.onrender.com',
     headers: "true"
 });
