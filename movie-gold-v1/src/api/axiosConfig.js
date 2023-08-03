fefc7129312dcfed1bd6ca37bb448f6d79773868
Jess Y <jyin978@hotmail.com>
2023-08-03 14:15:57 -0400

    testing api endpoints with axios and app

--- movie-gold-v1/src/api/axiosConfig.js
+++ movie-gold-v1/src/api/axiosConfig.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL:'http://localhost:8080',
-    baseURL: 'https://movie-gold-v2.onrender.com/api/v1/movies',
+    baseURL: 'https://movie-gold-v2.onrender.com',
 });
