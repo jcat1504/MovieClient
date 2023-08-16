040cb53262e9861ae261029a03730bab3267c20c
Jess Y <jyin978@hotmail.com>
2023-08-16 12:55:34 -0400

    changes to axios config file

--- movie-gold-v1/src/api/axiosConfig.js
+++ movie-gold-v1/src/api/axiosConfig.js
@@ -4,3 +4,5 @@ export default axios.create({
     // baseURL:'http://localhost:8080',
     baseURL: 'https://movie-gold-v2.onrender.com',
 });
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://movie-gold-v2.onrender.com/';
+
