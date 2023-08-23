ff39ee7885944bdeaabfc14aacff42f313b9ebde
Jess Y <jyin978@hotmail.com>
2023-08-23 11:20:50 -0400

    changes to links

--- movie-gold-v1/src/api/axiosConfig.js
+++ movie-gold-v1/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL:'http://localhost:8080',
-    baseURL: 'http://ec2-35-173-131-206.compute-1.amazonaws.com:8080/api/v1/movies',
+    baseURL: 'https://movie-gold-v2.onrender.com/',
 });
 
 
